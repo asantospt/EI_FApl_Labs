369ca87e74b602a6b01fa39793cd8c155fe6c9d4
a-santos <afonsosantos@gmail.com>
2017-12-05 01:41:40 +0000

    Conclusão do P4 e P5

--- Mod3_b/P4_Programa1/P4_Programa1.ino
+++ Mod3_b/P4_Programa1/P4_Programa1.ino
@@ -1,43 +1,49 @@
 /**
  * @ex - Mod.3a - P4
- * @brief - 
- * @date - 
+ * @brief - comentar o cod. do Prog. 1
+ * @date - 03/12
  * @author - Afonso & Natanael
- * @state - inc
+ * @state - ok
  */
 
-/*
-HC-SR04 distance sensor example
-Conections:
-VCC to arduino 5V
-GND to arduino GND
-Trig to Arduino pin 3
-Echo to Arduino pin 4
-*/
-
+// declaração de constantes, usadas no programa
 const int trigPin = 3;
 const int echoPin = 4;
 
+// função executada apenas 1x, quando Arduino é ligado à alimentação
 void setup() {
+  // define a velocidade da transmissão de dados da ligação Série
   Serial.begin (9600);
   
+  // define o tipo de Pin, INPUT ou OUTPUT
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   
+  // coloca o Pin off
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
 }
 
+// função executada sem parar, enquanto o Arduino tiver alimentação. É aqui que se colocam as instruções
 void loop() {
+  // declaração de 1 var. do tipo 'long'
   long duration;
   
+  // coloca o Pin on
   digitalWrite(trigPin, HIGH);
+  // espera durante 10 microssegundos
   delayMicroseconds(10);
+  // coloca o Pin off
   digitalWrite(trigPin, LOW);
   
+  // lê o pulso do Pin. Lê quanto tempo está a HIGH
   duration = pulseIn(echoPin, HIGH);
   
-  Serial.print(duration); //
+  // imprime na porta Série o valor da var.
+  Serial.print(duration); 
+
+  // imprime na porta Série a string
   Serial.println(" microssegundos");
+  // espera durante 1 segundo
   delay(1000);
 }
--- Mod3_b/P5_Programa1-edit/P5_Programa1-edit.ino
+++ Mod3_b/P5_Programa1-edit/P5_Programa1-edit.ino
@@ -1,9 +1,9 @@
 /**
  * @ex - Mod.3a - P5
- * @brief - 
- * @date - 
+ * @brief - Escrever alterações p/ mostrar a dist. p/ o objeto
+ * @date - 05/12
  * @author - Afonso & Natanael
- * @state - inc
+ * @state - ok
  */
 
 /*
@@ -31,13 +31,30 @@ void setup() {
 void loop() {
   long duration;
   
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(PIN_TRIG, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(PIN_TRIG, LOW);
   
-  duration = pulseIn(echoPin, HIGH);
-  
-  Serial.print(duration); //
+  duration = pulseIn(PIN_ECHO, HIGH);
+  Serial.print(duration); 
   Serial.println(" microssegundos");
+
+  /** ALTERAÇÕES: 
+   * Sabendo o tempo que demora o som a ir até ao objeto e voltar ao sensor (usando a função'pulseIn'), conseguimos 
+   *      calcular a distância a que se encontra esse objeto. 
+   * Usamos no cálculo a Vel. de propagação dos Ultrassons no Ar e uma simplificação da fórmula: 
+   * 2d = DeltaT(zero) * Vel.Ultrassons
+   */
+
+   // Solução - Afonso
+  float distance = (((float)duration * (float)VEL_ULTRASSONS_AR) / 2.0) / 10000.0;  
+
+  //2 Soluções - Natanael
+  //float distance = ((float)duration / 58.31);
+  //float distance = ((float)duration * 0,0172);
+
+  // Imprimir a distância no monitor Série
+  Serial.print(distance); 
+  Serial.println(" cm");
   delay(1000);
 }
